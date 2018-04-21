2f282c8cd2ca9fa692699c686a4bfda6e3345750
Penglei Yu <penglei@PengleideMBP.lan>
2018-04-21 14:14:48 +0800

    添加了算法库

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,5 +1,8 @@
+import edu.princeton.cs.algs4.StdOut;
+
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        StdOut.print("Hello");
     }
 }
