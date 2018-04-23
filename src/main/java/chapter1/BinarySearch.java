e9b8b1b49233025e738e63b77dda40fc79a1de4c
penglei <yupengniaosanshi@gmail.com>
2018-04-23 08:13:32 +0800

    二分查找添加了递归版本

--- src/main/java/chapter1/BinarySearch.java
+++ src/main/java/chapter1/BinarySearch.java
@@ -5,6 +5,11 @@ import edu.princeton.cs.algs4.StdIn;
 
 import java.util.Arrays;
 
+/**
+ * 练习命令行执行
+ * java_yu chapter1/BinarySearch tinyW.txt < tinyT.txt
+ * 编译和执行时都要通过classpath选项指定依赖库
+ */
 public class BinarySearch {
     /**
      * 先读取白名单，然后读取key列表，将不在白名单的key打印
@@ -26,19 +31,27 @@ public class BinarySearch {
 
     /**
      * 二分查找
-     *
-     * @param key key
-     * @param arr arr
-     * @return rank
      */
     private static int rank(int key, int[] arr) {
-        int lo = 0, hi = arr.length - 1;
-        while (lo <= hi) {
-            int mid = (lo + hi) / 2;
-            if (arr[mid] < key) lo = mid + 1;
-            else if (arr[mid] > key) hi = mid - 1;
-            else return mid;
-        }
-        return -1;
+//        int lo = 0, hi = arr.length - 1;
+//        while (lo <= hi) {
+//            int mid = (lo + hi) / 2;
+//            if (arr[mid] < key) lo = mid + 1;
+//            else if (arr[mid] > key) hi = mid - 1;
+//            else return mid;
+//        }
+//        return -1;
+        return rankR(key, arr, 0, arr.length - 1);
+    }
+
+    /**
+     * 二分查找的递归版本
+     */
+    private static int rankR(int key, int[] arr, int lo, int hi) {
+        if (lo > hi) return -1;
+        int mid = (lo + hi) / 2;
+        if (key > arr[mid]) return rankR(key, arr, mid + 1, hi);
+        else if (key < arr[mid]) return rankR(key, arr, lo, mid - 1);
+        else return mid;
     }
 }
